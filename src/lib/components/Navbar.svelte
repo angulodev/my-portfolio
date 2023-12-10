ccf2f8138255afc8448e332dc0b2067b71a74789
angulodev <francisco.angulo1992@gmail.com>
2023-12-10 19:57:33 -0300

    add:navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,52 +1,43 @@
 <script>
   import {
     IconMenu2,
-    IconMail,
     IconHomeHeart,
     IconPresentationAnalytics,
     IconFileCertificate,
     IconBriefcase,
     IconCode,
   } from "@tabler/icons-svelte";
+  let links = [
+    { name: "Home", url: "/", icon: IconHomeHeart },
+    {
+      name: "Experiencia",
+      url: "/experience",
+      icon: IconPresentationAnalytics,
+    },
+    { name: "Estudios", url: "/studies", icon: IconFileCertificate },
+    { name: "zona de juegos", url: "/sandbox", icon: IconBriefcase },
+    { name: "Contact", url: "/contact", icon: IconCode },
+  ];
 </script>
 
-<header class="flex h-full justify-between m-4">
-  <div class="flex items-center gap-4">
-    <div class="border rounded-md cursor-pointer">
-      <IconMenu2 size={48}></IconMenu2>
-    </div>
-    <p class="text-sm">AnguloDEV</p>
-  </div>
-  <div class="flex items-center gap-4">
-    <IconMail size={32}></IconMail>
-    <a href="/">
-      <img
-        class="inline-block h-12 w-12 rounded-full ring-2 ring-white"
-        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-        alt=""
-      />
-    </a>
-  </div>
-</header>
-
-<section class="mx-4">
-  <nav class="flex gap-4 justify-around">
-    <a href="/" class="flex items-center gap-4"
-      ><IconHomeHeart size={24}></IconHomeHeart> Home</a
-    >
-    <a href="/" class="flex items-center gap-4">
-      <IconPresentationAnalytics size={24}></IconPresentationAnalytics>
-      Experiencia</a
-    >
-    <a href="/" class="flex items-center gap-4">
-      <IconFileCertificate size={24}></IconFileCertificate>
-      Estudios</a
-    >
-    <a href="/" class="flex items-center gap-4">
-      <IconCode size={24}></IconCode> Sandbox</a
-    >
-    <a href="/" class="flex items-center gap-4"
-      ><IconBriefcase size={24}></IconBriefcase>Proyectos</a
-    >
-  </nav>
+<section class="flex justify-between">
+  <button class="text-2xl bg-transparent border-none cursor-pointer">
+    <IconMenu2></IconMenu2>
+  </button>
+  <img
+    src="/profile.jpg"
+    alt="una imagen de mix"
+    class="w-12 h-12 rounded-full"
+  />
 </section>
+
+<nav class="pt-4">
+  <ul class="flex justify-between">
+    {#each links as link}
+      <li>
+        <svelte:component this={link.icon} />
+        <a href={link.url} class="hidden sm:block"> {link.name}</a>
+      </li>
+    {/each}
+  </ul>
+</nav>
