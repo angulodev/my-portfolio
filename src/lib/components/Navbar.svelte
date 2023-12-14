25e1225993034713e715c908eeef71601821a296
angulodev <francisco.angulo1992@gmail.com>
2023-12-14 19:50:18 -0300

    Remove unnecessary code from Navbar component

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -20,17 +20,6 @@
   ];
 </script>
 
-<section class="flex justify-between m-8">
-  <button class="text-2xl bg-transparent border-none cursor-pointer">
-    <IconMenu2></IconMenu2>
-  </button>
-  <img
-    src="/profile.jpg"
-    alt="una imagen de mix"
-    class="w-12 h-12 rounded-full"
-  />
-</section>
-
 <nav class="py-6 mx-8">
   <ul class="flex justify-between">
     {#each links as link}
