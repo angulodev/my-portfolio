f64d93dfe6925391813547268ffaefccd3c7d305
angulodev <francisco.angulo1992@gmail.com>
2023-12-14 22:09:37 -0300

    Update profile image size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 <main class="flex flex-col items-center justify-center text-center">
   <h1 class="text-2xl mb-4">{name}</h1>
   <p class="text-lg mb-8">{description}</p>
-  <img src="/profile.jpg" alt="Avatar" class="w-24 h-24 rounded-full" />
+  <img src="/profile.jpg" alt="Avatar" class="w-48 h-48 rounded-full" />
 </main>
 <section class="flex gap-4 flex-col py-4">
   <p>
@@ -15,21 +15,6 @@
     apasiona la transformación digital y la mejora continua de los procesos y
     servicios de TI. Tengo conocimientos básicos en frameworks de JavaScript
     como Vue y React, y un nivel avanzado en Angular.
-  </p>
-
-  <p>
-    También domino las herramientas y metodologías ágiles como Scrum, Kanban,
-    Git y Jira. Me considero una persona proactiva, creativa y orientada al
-    cliente. Me gusta trabajar en equipo y aprender constantemente.
-  </p>
-  <p>
-    Mi objetivo es aportar valor a las organizaciones mediante el uso eficiente
-    y estratégico de Servicenow. Actualmente me encuentro trabajando en una
-    empresa líder en el sector de Servicenow, pero me gusta mantenerme al día de
-    las novedades y compartir mis conocimientos con la comunidad. Si tienes
-    alguna consulta o sugerencia sobre Servicenow, no dudes en contactarme.
-    Gracias por visitar mi perfil. Espero que te haya gustado lo que has leído.
-    😊
   </p>
 </section>
 <div class="flex gap-4 pb-8 justify-center">
